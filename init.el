3e2b9658060a3a0bc320fe163ec102c1d2d83555
Little Clover <littleclover909@gmail.com>
2022-07-08 21:18:12 +0800

    add julia-mode
    Reorganise `config.org` to include all programming language modes
    under a section.

--- init.el
+++ init.el
@@ -11,17 +11,22 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit rust-mode slime elfeed dashboard which-key poet-theme org-bullets parinfer-rust-mode use-package org-journal org ledger-mode)))
+   '(julia-mode magit rust-mode slime elfeed dashboard which-key poet-theme org-bullets parinfer-rust-mode use-package org-journal org ledger-mode)))
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
-(custom-set-faces
+(custom-set-faces)
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
